160d5113a850316404c949ad968eea782fde1d52
soynjoy <smilsohya@gmail.com>
2022-10-31 13:59:27 +0900

    first

---
+++ main.js
@@ -0,0 +1,12 @@
+$(function(){
+    $('#fullpage').fullpage({
+		//options here
+		autoScrolling:true,
+		scrollHorizontally: true,
+		navigation: true,
+		navigationPosition: 'right',
+		navigationTooltips: ['Background', 'Desk Research', 'Affinity Diagram', 'Persona & Journey Map', 'Persona & Journey Map', 'Problem & Solution', 
+		'Wire Frame & User Flow', 'Style Guide', 'Onboarding', 'Home', 'Diary', 'Diary', 'Diary', 'Recipe', 'Recipe', 'Community', 'Community']
+		
+	});
+});
